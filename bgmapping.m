c33afe96ff43f4493ec2cbad539440db16e6d0a4
Salman <kahn@berkeley.edu>
2020-05-07 02:16:51 -0700

    added new mapping program to plot b varying data

--- bgmapping.m
+++ bgmapping.m
@@ -1,12 +1,12 @@
-function [ output_args ] = gbmapping(start_file, Bstart, delB, Bfinal)
+function [ output_args ] = gbmapping(start_file, vgstart, delvg, vgfinal)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 
 % Counters
 i = start_file; %File Counter
 counter = 0; %Loop Counter
-numfiles = abs((Bfinal - Bstart))./abs(delB)+1; %Number of Files
-B = Bstart; %Initial Magnetic Field
+numfiles = abs((vgfinal - vgstart))./abs(delvg)+1; %Number of Files
+vg = vgstart; %Initial Magnetic Field
 
 for k = [1:1:numfiles]
     filename = sprintf('exp%d',i);
@@ -14,69 +14,104 @@ for k = [1:1:numfiles]
         load(['data_' filename '.mat']) 
     else
         filename =filesearch(filename);
-        data = transportdata(filename,0.00005,15,'s'); 
+        data = btransportdata(filename,0.00005,15,'s'); 
     end 
+    
+    B = data.B;
+    len(:,k) = length(data.B);
+    
+    counter = 1;
 
-    gate(:,k) = data.gate;
-    n(:,k) = data.n;
-    r(:,k) = data.r;
-    g(:,k) = data.g; 
-    Bf(:,k) = ones(size(gate,1),1).*B;
-    drdv(:,k) = data.drdv;
+    i = i + 3;
+end
+
+maxdatasize = max(len);
+
+B = NaN(maxdatasize,k);r = zeros(maxdatasize,k);g = zeros(maxdatasize,k);
+vgf = NaN(maxdatasize,k);drdv = zeros(maxdatasize,k);
+r2 = zeros(maxdatasize,k);dr2dv = zeros(maxdatasize,k);
+g2 = zeros(maxdatasize,k);dg2dv = zeros(maxdatasize,k);
+r3 = zeros(maxdatasize,k);dr3dv = zeros(maxdatasize,k);
+g3 = zeros(maxdatasize,k);dg3dv = zeros(maxdatasize,k);
+
+i = start_file; %File Counter
+counter = 0; %Loop Counter
+
+for k = [1:1:numfiles]
+    
+        filename = sprintf('exp%d',i);
+    if isfile(['data_' filename '.mat']) == 1
+        load(['data_' filename '.mat']) 
+    else
+        filename =filesearch(filename);
+        data = btransportdata(filename,0.00005,15,'s'); 
+    end 
+    
+    B(1:len(k),k) = data.B;
+    %n(:,k) = data.n;
+    r(1:len(k),k) = data.r;
+    rfit(1:len(k),k) = data.rfit;
+    g(1:len(k),k) = data.g; 
+    vgf(1:len(k),k) = ones(len(k),1).*vg;
+    drdv(1:len(k),k) = data.drdv;
     
     numdata = 1; 
     
-    if numel(fields(data)) > 6
+    if numel(fields(data)) > 7
         
-        r2(:,k) = data.r2;
-        dr2dv(:,k) = data.dr2dv;
-        g2(:,k) = data.g2; 
-        dg2dv(:,k) = data.dg2dv;
+        r2(1:len(k),k) = data.r2;
+        r2fit(1:len(k),k) = data.r2fit;
+        dr2dv(1:len(k),k) = data.dr2dv;
+        g2(1:len(k),k) = data.g2; 
+        dg2dv(1:len(k),k) = data.dg2dv;
         
         numdata = 2;
         
-        if numel(fields(data)) > 10
+        if numel(fields(data)) > 11
             
-            r3(:,k) = data.r3;
-            dr3dv(:,k) = data.dr3dv;
-            g3(:,k) = data.g3; 
-            dg3dv(:,k) = data.dg3dv;
+            r3(1:len(k),k) = data.r3;
+            r3fit(1:len(k),k) = data.r3fit;
+            dr3dv(1:len(k),k) = data.dr3dv;
+            g3(1:len(k),k) = data.g3; 
+            dg3dv(1:len(k),k) = data.dg3dv;
             
             numdata = 3;
         end 
     end 
     
     counter = 1;
-    i = i + 4;
-    B = B + delB; 
+    i = i+ 0
+    i = i + 3;
+    vg = vg + delvg; 
 end
+
 
 
 hFig = figure('Name',['Data ' num2str(start_file)],'NumberTitle','off','Color', 'w');
 
-colorscheme = othercolor('RdBu10',1024);
+colorscheme = othercolor('Greys9',1024);
 flippedJetColorMap = (colorscheme);
 colormap(flippedJetColorMap);
 
 s = warning('off', 'MATLAB:uitabgroup:OldVersion');
 hTabGroup = uitabgroup('Parent',hFig);
 warning(s);
-hTabs(1) = uitab('Parent',hTabGroup, 'Title','Gate, B, R');
-hTabs(2) = uitab('Parent',hTabGroup, 'Title','Gate, B, log(R)');
-hTabs(3) = uitab('Parent',hTabGroup, 'Title','Gate, B, dR/dV');
-hTabs(4) = uitab('Parent',hTabGroup, 'Title','Gate, B, log(dR/dV)');
+hTabs(1) = uitab('Parent',hTabGroup, 'Title','vgf, B, R');
+hTabs(2) = uitab('Parent',hTabGroup, 'Title','vgf, B, log(R)');
+hTabs(3) = uitab('Parent',hTabGroup, 'Title','vgf, B, dR/dV');
+hTabs(4) = uitab('Parent',hTabGroup, 'Title','vgf, B, log(dR/dV)');
 
 
 set(hTabGroup, 'SelectedTab',hTabs(1));
 
         hAx = axes('Parent',hTabs(1));
-        %Gate vs R
+        %vgf vs R
         subplot(1,3,1)
-        map = pcolor(gate,Bf, r);
+        map = pcolor(vgf,B, r);
         set(map,'EdgeColor','none')
         shading interp;
         colorbar;
-        xlabel('Gate (V)') 
+        xlabel('vgf (V)') 
         ylabel('B (T)')
         zlabel('R (k\Ohm)')
         title('R')
@@ -84,22 +119,22 @@ set(hTabGroup, 'SelectedTab',hTabs(1));
         
        
         subplot(1,3,2)
-        map = pcolor(gate,Bf, r2);
+        map = pcolor(vgf,B, r2);
         set(map,'EdgeColor','none')
         shading interp;
         colorbar;
-        xlabel('Gate (V)') 
+        xlabel('vgf (V)') 
         ylabel('B (T)')
         zlabel('R (k\Ohm)')
         title('R1')
         pbaspect([1 1 1])
         
         subplot(1,3,3)
-        map = pcolor(gate,Bf, r3);
+        map = pcolor(vgf,B, r3);
         set(map,'EdgeColor','none')
         shading interp;
         colorbar;
-        xlabel('Gate (V)') 
+        xlabel('vgf (V)') 
         ylabel('B (T)')
         zlabel('R (k\Ohm)')
         title('R2')
@@ -107,48 +142,51 @@ set(hTabGroup, 'SelectedTab',hTabs(1));
 
         hAx = axes('Parent',hTabs(2));
         
-        %Gate vs Log(R)
+        %vgf vs Log(R)
         subplot(1,3,1)
-        map = pcolor(gate,Bf, log(r));
+        map = pcolor(vgf,1./B, rfit);
         set(map,'EdgeColor','none')
         shading interp;
         colorbar;
-        xlabel('Gate (V)') 
+        xlabel('vgf (V)') 
         ylabel('B (T)')
         zlabel('R (k\Ohm)')
         title('R')
         pbaspect([1 1 1])
-       
+        ylim([.1 .5])
+        
         subplot(1,3,2)
-        map = pcolor(gate,Bf, log(abs((r2))));
+        map = pcolor(vgf,1./B, r2fit);
         set(map,'EdgeColor','none')
         shading interp;
         colorbar;
-        xlabel('Gate (V)') 
+        xlabel('vgf (V)') 
         ylabel('B (T)')
         zlabel('R (k\Ohm)')
         title('R1')
         pbaspect([1 1 1])
+        ylim([.1 .5])
         
         subplot(1,3,3)
-        map = pcolor(gate,Bf, log(abs((r2))));
+        map = pcolor(vgf,1./B, r3fit);
         set(map,'EdgeColor','none')
         shading interp;
         colorbar;
-        xlabel('Gate (V)') 
+        xlabel('vgf (V)') 
         ylabel('B (T)')
         zlabel('R (k\Ohm)')
         title('R2')
         pbaspect([1 1 1]) 
-  
+         ylim([.1 .5])
+         
          hAx = axes('Parent',hTabs(3));
-        %Gate vs d(R)/dV
+        %vgf vs d(R)/dV
         subplot(1,3,1)
-        map = pcolor(gate,Bf, ((drdv)));
+        map = pcolor(vgf,B, ((drdv)));
         set(map,'EdgeColor','none')
         shading interp;
         colorbar;
-        xlabel('Gate (V)') 
+        xlabel('vgf (V)') 
         ylabel('B (T)')
         zlabel('R (k\Ohm)')
         title('R')
@@ -156,57 +194,57 @@ set(hTabGroup, 'SelectedTab',hTabs(1));
         
         
         subplot(1,3,2)
-        map = pcolor(gate,Bf, (((dr2dv))));
+        map = pcolor(vgf,B, (((dr2dv))));
         set(map,'EdgeColor','none')
         shading interp;
         colorbar;
-        xlabel('Gate (V)') 
+        xlabel('vgf (V)') 
         ylabel('B (T)')
         zlabel('R (k\Ohm)')
         title('R1')
         pbaspect([1 1 1])
         
         subplot(1,3,3)
-        map = pcolor(gate,Bf, (((dr3dv))));
+        map = pcolor(vgf,B, (((dr3dv))));
         set(map,'EdgeColor','none')
         shading interp;
         colorbar;
-        xlabel('Gate (V)') 
+        xlabel('vgf (V)') 
         ylabel('B (T)')
         zlabel('R (k\Ohm)')
         title('R2')
         pbaspect([1 1 1])         
 
         hAx = axes('Parent',hTabs(4));
-        %Gate vs d(R)/dV
+        %vgf vs d(R)/dV
         subplot(1,3,1)
-        map = pcolor(gate,Bf, log(abs(drdv)));
+        map = pcolor(vgf,B, log(abs(drdv)));
         set(map,'EdgeColor','none')
         shading interp;
         colorbar;
-        xlabel('Gate (V)') 
+        xlabel('vgf (V)') 
         ylabel('B (T)')
         zlabel('R (k\Ohm)')
         title('R')
         pbaspect([1 1 1])
         
         subplot(1,3,2)
-        map = pcolor(gate,Bf, log(abs((dr2dv))));
+        map = pcolor(vgf,B, log(abs((dr2dv))));
         set(map,'EdgeColor','none')
         shading interp;
         colorbar;
-        xlabel('Gate (V)') 
+        xlabel('vgf (V)') 
         ylabel('B (T)')
         zlabel('R (k\Ohm)')
         title('R1')
         pbaspect([1 1 1])
         
         subplot(1,3,3)
-        map = pcolor(gate,Bf, log(abs((dr3dv))));
+        map = pcolor(vgf,B, log(abs((dr3dv))));
         set(map,'EdgeColor','none')
         shading interp;
         colorbar;
-        xlabel('Gate (V)') 
+        xlabel('vgf (V)') 
         ylabel('B (T)')
         zlabel('R (k\Ohm)')
         title('R2')
---
+++ btransportdata.m
@@ -0,0 +1,78 @@
+function [ data ] = btransportdata(filename,vsd,do2,savedata)
+
+%transportdata - Converts text file from Wu Shi's ACDC aquisition program
+%   in a cell-array that contains converted data. This cell array contains
+%              - Gate Voltage (V): Gate applied to the device
+%              - n (electrons/cm^2): Carrier density of sample based on
+%              sample geometry. 
+%              - r (ohms): Resistance of channel based on vsd
+%              - g (siemens): Conductance of channel based on vsd 
+
+if ~exist('savedata','var')
+    % third parameter does not exist, so default it to something
+     savedata = 'ds';
+end
+% Math
+
+% Carrier Density Conversion 
+eo = 8.854.*10.^(-12); % F/m Electric Constant
+er1 = 4; % Dielectric Constant
+er2 = 4; 
+ec = 1.6021766.*10.^(-19); % C Charge of Electron 
+do1 = 285; % nm Dielectric Thickness Oxide
+%do2 =20; %nm Dielectric Thickness BN 
+d1 = do1.*10^(-9);
+d2 = do2.*10^(-9);
+con = 1./10000; % Nm to meV 
+h = 6.62607.*10.^(-34); % Js
+
+%Quantum Flux Calculation 
+phio = h/ec; %Magnetic Flux Quanta
+a = 11;
+A = a.^2*sqrt(3)./2; 
+
+% Data extraction
+dat = (csvread(filename,1));
+%if dat(1,1) >= dat(end,1)
+%    dat = flipud(dat); 
+%end 
+data = {};
+
+% Check for number of channels
+[~, channels] = size(dat);
+
+%Extrac primary channel data 
+data.B = dat(:,1)./10000; 
+%data.n = (dat(:,1)).*con.*(eo./ec).*(er1.*er2)./(d1.*er2 + d2.*er1); 
+data.g = dat(:,2)./vsd; 
+data.r = vsd./dat(:,2);
+data.rfit = data.r - polyval(polyfit(data.B,data.r,6),data.B);
+data.drdv = diff([eps; data.r])./diff([eps; data.B]);
+data.dgdv = diff([eps; data.g])./diff([eps; data.B]);
+
+%Extract other channel data
+
+if channels > 2
+
+    data.r2 = dat(:,3);
+    data.r2fit = data.r2 - polyval(polyfit(data.B,data.r2,6),data.B);
+    data.g2 = 1./data.r2;
+    data.dr2dv = diff([eps; data.r2])./diff([eps; data.B]);
+    data.dg2dv = diff([eps; data.g2])./diff([eps; data.B]); 
+    
+    if channels > 3
+        data.r3 = dat(:,4);
+        data.r3fit = data.r3 - polyval(polyfit(data.B,data.r3,6),data.B);
+        data.g3 = 1./data.r3;
+        data.dr3dv = diff([eps; data.r3])./diff([eps; data.B]);
+        data.dg3dv = diff([eps; data.g3])./diff([eps; data.B]);
+    end 
+end
+
+% Save data for later use
+if savedata == 's'
+    exportname = split(filename);
+    save(['data_' exportname{1} '.mat'],'data')
+end 
+end
+
