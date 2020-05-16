2b2a5038420da9ff2da04d2fecfaf35ed6da6dcd
Salman <kahn@berkeley.edu>
2020-05-16 16:01:25 -0700

    Updated program to include various inputs using varargin

--- bgmapping.m
+++ bgmapping.m
@@ -1,12 +1,24 @@
-function [dat] = bgmapping(start_file, vgstart, delvg, vgfinal,savedata)
+function [dat] = bgmapping(start_file, vgstart, delvg, vgfinal,varargin)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 
-% Saving Data Protocol
-if ~exist('savedata','var')
-    % third parameter does not exist, so default it to something
-     savedata = 'ds';
-end
+vsd = 0.0005; do2 = 20; savedata = 's'; a = 11; w = 1; l = 1;
+
+for j = [1:2:size(varargin,2)]
+    if strcmp(varargin{j},'vsd') == 1
+        vsd = varargin{j+1};
+    elseif strcmp(varargin{j},'bnthickness') == 1 
+        do2 = varargin{j+1};
+    elseif strcmp(varargin{j},'width') == 1 
+        w = varargin{j+1};
+    elseif strcmp(varargin{j},'length') == 1 
+        l = varargin{j+1};
+    elseif strcmp(varargin{j},'savedata') == 1 
+        savedata = varargin{j+1};
+    elseif strcmp(varargin{j},'moire') == 1 
+        a = varargin{j+1};
+    end 
+end 
 
 % Counters
 i = start_file; %File Counter
@@ -24,7 +36,7 @@ for k = [1:1:numfiles]
         load(['data_' filename '.mat']) 
     else
         filename =filesearch(filename);
-        data = btransportdata(filename,0.001,30,'s'); 
+        data = btransportdata(filename,'vsd',0.001,'bnthickness',30,'savedata','s'); 
     end 
     
     len(:,k) = length(data.B);
@@ -46,8 +58,9 @@ dat.r = NaN(maxdatasize,k); dat.drdB = NaN(maxdatasize,k);
 dat.g = NaN(maxdatasize,k);dat.dgdB = NaN(maxdatasize,k);
 
 dat.rfit = NaN(maxdatasize,k); dat.r2fit = NaN(maxdatasize,k); dat.r3fit = NaN(maxdatasize,k);
+dat.gfit = NaN(maxdatasize,k); dat.g2fit = NaN(maxdatasize,k); dat.g3fit = NaN(maxdatasize,k);
 
-dat.r2 = NaN(maxdatasize,k); dat.dr2dB = NaN(maxdatasize,k);
+dat.r2 = NaN(maxdatasize,k); dat.rho2 = NaN(maxdatasize,k); dat.dr2dB = NaN(maxdatasize,k);
 dat.g2 = NaN(maxdatasize,k); dat.dg2dB = NaN(maxdatasize,k);
 dat.r3 = NaN(maxdatasize,k); dat.dr3dB = NaN(maxdatasize,k);
 dat.g3 = NaN(maxdatasize,k); dat.dg3dB = NaN(maxdatasize,k);
@@ -68,44 +81,47 @@ for k = [1:1:numfiles]
     end 
     
     %Data extraction for plotting
-    xy = 1;
-    dat.B(1:len(k),k) = data.B; xyname{1,xy} = 'B (T)'; xyname{2,xy} = 'B';
-    dat.phiphio(1:len(k),k) = data.phiphio; xyname{1,xy+1} = [char(981) '/' char(981) 'o']; xyname{2,xy+1} = 'phiphio';
-    dat.phiophi(1:len(k),k) = data.phiophi; xyname{1,xy+2} = [char(981) 'o/' char(981)]; xyname{2,xy+2} = 'phiophi';
-    dat.Vg(1:len(k),k) = ones(len(k),1).*vgi; xyname{1,xy+3} = 'Vg (V)'; xyname{2,xy+3} = 'Vg';
-    dat.n(1:len(k),k) = data.n.*dat.Vg(1:len(k),k); xyname{1,xy+4} = ['n (1/cm' char(0178) ')']; xyname{2,xy+4} = 'n';
-    dat.nno(1:len(k),k) = dat.n(1:len(k),k)./12; xyname{1,xy+5} = 'n/n_{o}'; xyname{2,xy+5} = 'nno';
+
+    dat.B(1:len(k),k) = data.B; xyname.B = 'B (T)';
+    dat.phiphio(1:len(k),k) = data.phiphio; xyname.phiphio = '\phi/\phi_{o}';
+    dat.phiophi(1:len(k),k) = data.phiophi; xyname.phiophi = '\phi_{o}/\phi'; 
+    dat.Vg(1:len(k),k) = ones(len(k),1).*vgi; xyname.Vg = 'V_{g} (V)';
+    dat.n(1:len(k),k) = data.n.*dat.Vg(1:len(k),k); xyname.n = 'n (cm^{-2})'; 
+    dat.nno(1:len(k),k) = dat.n(1:len(k),k)./12; xyname.nno = 'n/n_{o}'; 
+
+    dat.r(1:len(k),k) = data.r; zname.r = 'r (\Ohms)';
+    dat.rfit(1:len(k),k) = data.rfit; zname.rfit = ['r_{fit}' ' (' char(911) ')']; 
+    dat.drdB(1:len(k),k) = data.drdB; zname.drdB = 'dr/dB';
     
-    z = 1;
-    dat.r(1:len(k),k) = data.r; zname{1,z} = ['r' ' (' char(911) ')']; zname{2,z} = 'r'; 
-    dat.rfit(1:len(k),k) = data.rfit; zname{1,z+1} = ['r_{fit}' ' (' char(911) ')']; zname{2,z+1} = 'rfit';
-    dat.drdB(1:len(k),k) = data.drdB; zname{1,z+2} = 'dr/dB'; zname{2,z+2} = 'drdB';
-    
-    dat.g(1:len(k),k) = data.g; zname{1,z+3} = 'g (S)'; zname{2,z+3} = 'g';
-    dat.dgdB(1:len(k),k) = data.dg2dB; zname{1,z+4} = 'dg/dB'; zname{2,z+4} = 'dgdB';
+    dat.g(1:len(k),k) = data.g; zname.g = 'g (S)';
+    dat.gfit(1:len(k),k) = data.gfit; zname.gfit = ['g_{fit}' ' (S)'];
+    dat.dgdB(1:len(k),k) = data.dg2dB; zname.dgdB = 'dg/dB';
     
     
     numdata = 1; 
     
     if numel(fields(data)) > 7
         
-        dat.r2(1:len(k),k) = data.r2; zname{1,z+5} = ['r_{2}' ' (' char(911) ')']; zname{2,z+5} = 'r2';
-        dat.r2fit(1:len(k),k) = data.r2fit; zname{1,z+6} = 'r2fit (Ohms)'; zname{2,z+6} = 'r2fit';
-        dat.dr2dB(1:len(k),k) = data.dr2dB; zname{1,z+7} = 'dr2dB (Ohms)'; zname{2,z+7} = 'dr2dB';
+        dat.r2(1:len(k),k) = data.r2; zname.r2 = ['r_{2}' ' (' char(911) ')'];
+        dat.rho2(1:len(k),k) = data.rho2; zname.rho2 = ['r_{2}' ' (' char(911) ')'];
+        dat.r2fit(1:len(k),k) = data.r2fit; zname.r2fit = 'r2fit (Ohms)'; 
+        dat.dr2dB(1:len(k),k) = data.dr2dB; zname.dr2dB = 'dr2dB (Ohms)';
         
-        dat.g2(1:len(k),k) = data.g2; zname{1,z+8} = 'g2 (S)'; zname{2,z+8} = 'g2';
-        dat.dg2dB(1:len(k),k) = data.dg2dB; zname{1,z+9} = 'dg2/dB'; zname{2,z+9} = 'dg2dB';
+        dat.g2(1:len(k),k) = data.g2; zname.g2 = 'g2 (S)';
+        dat.g2fit(1:len(k),k) = data.g2fit; zname.g2fit = ['g2_{fit}' ' (S)'];
+        dat.dg2dB(1:len(k),k) = data.dg2dB; zname.dg2dB = 'dg2/dB';
         
         numdata = 2;
         
         if numel(fields(data)) > 11
             
-            dat.r3(1:len(k),k) = data.r3; zname{1,z+10} = ['r3' ' (' char(911) ')']; zname{2,z+10} = 'r3';
-            dat.r3fit(1:len(k),k) = data.r3fit; zname{1,z+11} = ['r3fit' ' (' char(911) ')']; zname{2,z+11} = 'r3fit';
-            dat.dr3dB(1:len(k),k) = data.dr3dB; zname{1,z+12} = ['dr3dB' ' (' char(911) ')']; zname{2,z+12} = 'dr3dB';
+            dat.r3(1:len(k),k) = data.r3; zname.r3 = ['r3' ' (' char(911) ')'];
+            dat.r3fit(1:len(k),k) = data.r3fit; zname.r3fit = ['r3fit' ' (' char(911) ')']; 
+            dat.dr3dB(1:len(k),k) = data.dr3dB; zname.dr3dB = ['dr3dB' ' (' char(911) ')']; 
             
-            dat.g3(1:len(k),k) = data.g3; zname{1,z+13} = 'g3 (S)'; zname{2,z+13} = 'g3';
-            dat.dg3dB(1:len(k),k) = data.dg3dB; zname{1,z+14} = 'dg3/dB'; zname{2,z+14} = 'dg3dB';
+            dat.g3(1:len(k),k) = data.g3; zname.g3 = 'g3 (S)'; 
+            dat.g3fit(1:len(k),k) = data.g3fit; zname.g3fit = ['g3_{fit}' ' (S)'];
+            dat.dg3dB(1:len(k),k) = data.dg3dB; zname.dg3dB = 'dg3/dB';
             
             numdata = 3;
         end 
@@ -118,8 +134,9 @@ for k = [1:1:numfiles]
     vgi = vgi + delvg; 
 end
 
-dat.xyname = xyname;
-dat.zname = zname;
+dat.xy = xyname;
+dat.z = zname;
+dat.title = ['Map of exp' num2str(start_file) ' to exp' num2str((i-3))];
 
 if savedata == 'ds'
     
